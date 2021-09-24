e94c6256f46422b3051db29df1d5cd70d4b0ec5b
Wilasinee Wiwattara <wilasinee_wiw61@tbs.tu.ac.th>
2021-09-24 15:51:19 +0700

    Create midterm_app for exam

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'models/form_model.dart';
 import 'pages/first_page.dart';
 import 'pages/second_page.dart';
 import 'pages/third_page.dart';
@@ -9,7 +11,13 @@ import 'pages/sixth_page.dart';
 import 'pages/seventh_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (context) => FormModel(),
+      ),
+    ], child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
